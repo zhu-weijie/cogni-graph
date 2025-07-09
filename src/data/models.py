54385d62cec551e26fed78f330a7d79c711e63be
zhuwj <byzhubaiyuan@gmail.com>
2025-07-09 12:28:35 +0800

    Define database table

---
+++ src/data/models.py
@@ -0,0 +1,16 @@
+import uuid
+
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+
+class Tenant(Base):
+    __tablename__ = "tenants"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
