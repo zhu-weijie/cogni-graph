8b5302ea2ef97cc0844dbdbd1e3a7646afbcc282
zhuwj <byzhubaiyuan@gmail.com>
2025-07-09 13:31:54 +0800

    Add the Service Function

--- src/core/services/tenant_service.py
+++ src/core/services/tenant_service.py
@@ -4,6 +4,10 @@ from src.api.schemas import TenantCreate
 from src.data.models import Tenant
 
 
+def get_tenants(db: Session) -> list[Tenant]:
+    return db.query(Tenant).all()
+
+
 def create_tenant(db: Session, tenant: TenantCreate) -> Tenant:
     db_tenant = Tenant(name=tenant.name)
     db.add(db_tenant)
