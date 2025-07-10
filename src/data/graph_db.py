a9ff904f271571eba2cf97503913b8c056a0ca90
zhuwj <byzhubaiyuan@gmail.com>
2025-07-10 09:54:07 +0800

    Create Graph Database Connection Module

---
+++ src/data/graph_db.py
@@ -0,0 +1,29 @@
+from neo4j import GraphDatabase
+
+from src.config import settings
+
+
+class GraphDB:
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self._driver.close()
+
+    def get_session(self):
+        return self._driver.session()
+
+
+graph_db_instance = GraphDB(
+    uri=settings.NEO4J_URI,
+    user=settings.NEO4J_USER,
+    password=settings.NEO4J_PASSWORD,
+)
+
+
+def get_graph_session():
+    session = graph_db_instance.get_session()
+    try:
+        yield session
+    finally:
+        session.close()
