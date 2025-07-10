b441bdbc6a8174c547f98b5041cc191c6c01c183
zhuwj <byzhubaiyuan@gmail.com>
2025-07-10 13:17:50 +0800

    Implement graph qa service

--- src/core/services/qa_service.py
+++ src/core/services/qa_service.py
@@ -1,13 +1,15 @@
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chains.graph_qa.cypher import GraphCypherQAChain
 from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_neo4j import Neo4jGraph
 from langchain_openai import ChatOpenAI
 
 from src.config import settings
 
 llm = ChatOpenAI(openai_api_key=settings.OPENAI_API_KEY, model="gpt-4o")
 
-prompt = ChatPromptTemplate.from_template(
+rag_prompt = ChatPromptTemplate.from_template(
     """Answer the following question based only on the provided context.
 If you don't know the answer, just say that you don't know. Don't try
 to make up an answer.
@@ -18,10 +20,25 @@ to make up an answer.
 
 Question: {input}"""
 )
-
-qa_chain = create_stuff_documents_chain(llm, prompt)
+rag_qa_chain = create_stuff_documents_chain(llm, rag_prompt)
 
 
 def generate_answer(query: str, context: list[Document]) -> str:
-    response = qa_chain.invoke({"input": query, "context": context})
+    response = rag_qa_chain.invoke({"input": query, "context": context})
     return response
+
+
+graph = Neo4jGraph(
+    url=settings.NEO4J_URI,
+    username=settings.NEO4J_USER,
+    password=settings.NEO4J_PASSWORD,
+)
+
+graph_qa_chain = GraphCypherQAChain.from_llm(
+    graph=graph, llm=llm, verbose=True, allow_dangerous_requests=True
+)
+
+
+def generate_graph_answer(query: str) -> str:
+    response = graph_qa_chain.invoke({"query": query})
+    return response["result"]
