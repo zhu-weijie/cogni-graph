337988cbf92486d333eefa157af0805375d5124d
zhuwj <byzhubaiyuan@gmail.com>
2025-07-12 15:24:57 +0800

    Give GitHub Actions role permission to read the specific secret

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region for the infrastructure."
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "aws_account_id" {
+  description = "The AWS account ID."
+  type        = string
+  default     = "215288576473"
+}
