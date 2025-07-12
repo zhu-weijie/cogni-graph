9863f0b3bb2fc23fd64531fb7d7c9de6b399b59f
zhuwj <byzhubaiyuan@gmail.com>
2025-07-12 13:28:34 +0800

    Add Terraform Outputs, add actions repository secrets

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository for the API."
+  value       = aws_ecr_repository.api.repository_url
+}
+
+output "oidc_role_arn" {
+  description = "The ARN of the IAM role for GitHub Actions CD."
+  value       = aws_iam_role.github_actions_cd.arn
+}
