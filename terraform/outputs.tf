93227478a9fb5fddf03d72d3f9ae65b7e912e1dc
zhuwj <byzhubaiyuan@gmail.com>
2025-07-13 20:43:48 +0800

    Output the ARN of the auto-generated secret

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "oidc_role_arn" {
   description = "The ARN of the IAM role for GitHub Actions CD."
   value       = aws_iam_role.github_actions_cd.arn
 }
+
+output "rds_master_password_secret_arn" {
+  description = "The ARN of the automatically generated RDS master password secret."
+  value       = aws_db_instance.main.master_user_secret[0].secret_arn
+}
