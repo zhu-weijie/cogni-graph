2f2c0b66e6bf348bef59df8c0a0523995c8aa383
zhuwj <byzhubaiyuan@gmail.com>
2025-07-13 18:17:00 +0800

    Remove the data sources entirely and configure the kubernetes provider block directly

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -31,20 +31,14 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_eks_cluster" "cluster" {
+data "aws_eks_cluster_auth" "main" {
   name = aws_eks_cluster.main.name
-  depends_on = [aws_eks_cluster.main]
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.main.name
-  depends_on = [aws_eks_cluster.main]
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
 }
 
 provider "helm" {}
