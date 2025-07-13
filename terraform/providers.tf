e4b7626613d9bbc09bc1c7395c01b45eb1d37afd
zhuwj <byzhubaiyuan@gmail.com>
2025-07-13 17:26:46 +0800

    Update providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,38 @@
-provider "aws" {
-  region = "ap-southeast-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+  }
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.main.name
+  depends_on = [aws_eks_cluster.main]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.main.name
+  depends_on = [aws_eks_cluster.main]
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {}
