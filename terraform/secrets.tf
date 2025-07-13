a74ab68c112f2b7fc8314a392644781d3f0431b2
zhuwj <byzhubaiyuan@gmail.com>
2025-07-13 17:37:22 +0800

    Add secret for Neo4j

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,4 +1,23 @@
 resource "aws_secretsmanager_secret" "db_password" {
-  name = "cogni-graph-db-password"
-  description = "Password for the CogniGraph RDS database"
+  name        = "cogni-graph-db-password"
+  description = "Password for the CogniGraph RDS database managed by RDS"
+}
+
+resource "aws_secretsmanager_secret" "neo4j_password" {
+  name        = "cogni-graph-neo4j-password"
+  description = "Password for the Neo4j database"
+}
+
+resource "aws_secretsmanager_secret_version" "neo4j_password_version" {
+  secret_id = aws_secretsmanager_secret.neo4j_password.id
+
+  secret_string = jsonencode({
+    password = random_password.neo4j.result
+  })
+}
+
+resource "random_password" "neo4j" {
+  length           = 16
+  special          = true
+  override_special = "!#%&"
 }
